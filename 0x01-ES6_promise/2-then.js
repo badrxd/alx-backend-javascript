28bea3e727d53568f4b220cb18d7f7a441e617bb
badrxd <badrxdxd@gmail.com>
2024-01-03 12:55:59 +0100

    task 2. Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,6 @@
+export default async function handleResponseFromAPI(promise) {
+  await promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => Error(''))
+    .finally(() => { console.log('Got a response from the API'); });
+}
