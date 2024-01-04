f20d404eddd29ebe1c7548a5a3ef5b1fae375925
badrxd <badrxdxd@gmail.com>
2024-01-04 12:44:14 +0100

    task 1. Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,7 +2,7 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms(second) {
   let i = 0;
-  const obj = ['19', '20', '34'];
+  const obj = [19, 20, 34];
   while (i < 3) {
     obj[i] = new ClassRoom(obj[i]);
     i++;
