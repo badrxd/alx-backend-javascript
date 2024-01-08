fe946fbc7d3ed9be7e3e42322ddb267373d8a374
badrxd <badrxdxd@gmail.com>
2024-01-08 14:42:01 +0100

    task 1. More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 const getListStudentIds = (arr) => {
-  const b = [...arr];
   const c = [];
-  b.map((e) => (e.id !== undefined ? c.push(e.id) : null));
+  Array.from(arr).map((e) => (e.id !== undefined ? c.push(e.id) : null));
   return c;
 };
 export default getListStudentIds;
