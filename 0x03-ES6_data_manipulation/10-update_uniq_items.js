92cb5ca4fa353942033c244cad451d61bf7f1470
badrxd <badrxdxd@gmail.com>
2024-01-09 13:30:50 +0100

    task 10. More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,6 @@
+export default function updateUniqueItems(map) {
+  if (map === undefined || map.size < 1) {
+    throw Error('Cannot process');
+  }
+  map.forEach((v, k, map) => { v == 1 ? map.set(k, 100) : null; });
+}
