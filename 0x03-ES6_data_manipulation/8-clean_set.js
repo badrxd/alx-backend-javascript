369137ba17d22763ab58e0666b8d3ae6cb9690f3
badrxd <badrxdxd@gmail.com>
2024-01-09 12:43:09 +0100

    ach had sba7 dyal lkhra

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (startString === undefined || startString.length === 0) return '';
   return Array.from(set)
-    .filter((e) => e.slice(0, startString.length) === startString)
+    .filter((e) => e.slice(0, startString.length) === startString && e.length > startString.length)
     .map((e) => e.slice(startString.length))
     .join('-');
 }
--- test.js
+++ test.js
@@ -1,28 +1,7 @@
-const c = [-2, 1, -3, 4, -1, 2, 1, -5, 4, [0, 1, 2, [3, 4]]]
-// console.log(b);
-// console.log(c);
-
-// const months = ["Mar", "Jan", "Feb", "Dec"];
-// const sortedMonths = months.toSorted();
-// console.log(sortedMonths);
-function myFunction() {
-    console.time('myFunction');
-    // let b = [1, 5, 7, 10, 12, 14, 15, 18, 20,
-    //     22, 25, 27, 300, 64, 110, 220]
-    const b = new Int16Array(c.length)
-    // console.log(b[5]); 
-
-    // Start the timer
-    let e = 1
-
-    for (let i = 0; i < c.length; i++) {
-        b[i] = c[i] * 2
-    }
-    console.log(b);
-
-    // End the timer
-    console.timeEnd('myFunction');
-}
-
-// Call the function
-myFunction();
+const buffer = new ArrayBuffer(10);
+const uint8 = new Uint8Array(buffer);
+// Data manually written here, but pretend it was already in the buffer
+uint8.set([228, 189, 160, 229, 165, 189]);
+const text = new TextDecoder().decode(uint8);
+console.log(text);
+console.log(buffer);
