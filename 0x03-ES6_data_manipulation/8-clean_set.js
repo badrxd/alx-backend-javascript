bea86a656a257ec0e72cc3d06aa302f7ace89f45
badrxd <badrxdxd@gmail.com>
2024-01-09 12:32:09 +0100

    fix length

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,3 @@
 export default function cleanSet(set, startString) {
-  if (set.size < 1) {
-    return ''
-  }
-  return Array.from(set).filter((e) => e.slice(0, 3) === startString && e.length > 3).map((e) => e.slice(3)).join('-');
+  return Array.from(set).filter((e) => e.slice(0, 3) === startString && e.length > startString.length).map((e) => e.slice(3)).join('-');
 }
