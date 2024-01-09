475283d1a979afd9a2c89296e7a589ef2bc5b0d1
badrxd <badrxdxd@gmail.com>
2024-01-09 14:10:14 +0100

    problem in if statment

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
-    throw Error('Position outside range');
-  }
+  if (position >= length) throw Error('Position outside range');
 
   const Int8 = new DataView(new ArrayBuffer(length));
   Int8.setInt8(position, value);
