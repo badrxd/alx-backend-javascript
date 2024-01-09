b7576fc3ad519b7683461d2c6be9c6401ebc3f9a
badrxd <badrxdxd@gmail.com>
2024-01-09 14:02:37 +0100

    add more cheackers

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === undefined || startString.length === 0 || !(set instanceof Set)) return '';
+  if (typeof startString !== 'string' || startString === undefined || set === undefined || startString.length === 0 || !(set instanceof Set)) return '';
   return Array.from(set)
     .filter((e) => e.slice(0, startString.length) === startString && e.length > startString.length)
     .map((e) => e.slice(startString.length))
