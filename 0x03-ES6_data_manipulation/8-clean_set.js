2e3796724fc1bd57c786c7693ba73bd658f35f00
badrxd <badrxdxd@gmail.com>
2024-01-09 12:10:17 +0100

    task 8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,3 @@
 export default function cleanSet(set, startString) {
-  return Array.from(set).filter((e) => e.slice(0, 3) === startString).map((e) => e.slice(3)).join('_');
+  return Array.from(set).filter((e) => e.slice(0, 3) === startString).map((e) => e.slice(3)).join('-');
 }
