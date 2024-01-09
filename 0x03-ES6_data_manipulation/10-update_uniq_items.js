57901f48ccbf64cbca48b57f3ec7491f0ee84a5c
badrxd <badrxdxd@gmail.com>
2024-01-09 13:48:05 +0100

    check if map instanceof Map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-  if (map === undefined || map.size < 1) {
+  if (map === undefined || map instanceof Map === false) {
     throw Error('Cannot process');
   }
   map.forEach((v, k, map) => (v === 1 ? map.set(k, 100) : null));
