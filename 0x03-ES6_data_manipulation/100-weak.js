9e9021048bc6c9dff57993aafc40becd3f671c87
badrxd <badrxdxd@gmail.com>
2024-01-09 16:10:48 +0100

    fix style

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,18 +1,18 @@
 const weakMap = new WeakMap();
 
 const queryAPI = (obj) => {
-    if (!obj || !(obj instanceof Object)) {
-        return;
-    }
-    if (!weakMap.has(obj)) {
-        weakMap.set(obj, 0);
-    }
-    const req = weakMap.get(obj);
-    if (req < 4) {
-        weakMap.set(obj, (req + 1));
-    } else {
-        throw new Error('Endpoint load is high');
-    }
+  if (!obj || !(obj instanceof Object)) {
+    return;
+  }
+  if (!weakMap.has(obj)) {
+    weakMap.set(obj, 0);
+  }
+  const req = weakMap.get(obj);
+  if (req < 4) {
+    weakMap.set(obj, (req + 1));
+  } else {
+    throw new Error('Endpoint load is high');
+  }
 };
 
 export { weakMap, queryAPI };
