37542233083ee986bb8bb5126fa298a65d47d6e4
badrxd <badrxdxd@gmail.com>
2024-01-09 12:35:24 +0100

    same error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,7 @@
 export default function cleanSet(set, startString) {
-  return Array.from(set).filter((e) => e.slice(0, 3) === startString && e.length > startString.length).map((e) => e.slice(3)).join('-');
+  if (startString === undefined || startString.length === 0) return '';
+  return Array.from(set)
+    .filter((e) => e.slice(0, 3) === startString > startString.length)
+    .map((e) => e.slice(3))
+    .join('-');
 }
