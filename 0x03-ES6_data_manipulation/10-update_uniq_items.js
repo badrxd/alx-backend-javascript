8864fcb6b275a5b9abef7da65fe25d6124f50c2f
badrxd <badrxdxd@gmail.com>
2024-01-09 13:32:20 +0100

    fix style error

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,5 +2,5 @@ export default function updateUniqueItems(map) {
   if (map === undefined || map.size < 1) {
     throw Error('Cannot process');
   }
-  map.forEach((v, k, map) => { v == 1 ? map.set(k, 100) : null; });
+  map.forEach((v, k, map) => (v === 1 ? map.set(k, 100) : null));
 }
