6d5bc601c77700bf48eefaa802d90de67621908d
badrxd <badrxdxd@gmail.com>
2024-01-09 14:22:06 +0100

    d

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === undefined || set === undefined|| !(set instanceof Set) || typeof startString !== 'string') return '';
+  if (startString === undefined || set === undefined || !(set instanceof Set) || typeof startString !== 'string') return '';
   return Array.from(set)
     .filter((e) => e.slice(0, startString.length) === startString && e.length > startString.length)
     .map((e) => e.slice(startString.length))
