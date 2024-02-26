2409c051769eaa84ba44abce1ca9f5836270e68f
badrxd <badrxdxd@gmail.com>
2024-02-26 23:45:45 +0100

    task 2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const fields = new Map();
+    let NumStd = 0;
+
+    let contents = fs.readFileSync(path, 'utf8');
+    contents = contents.split('\n');
+    for (let i = 0; i < contents.length; i += 1) {
+      if (contents[i].length > 0 && contents[i] !== 'firstname,lastname,age,field') {
+        const data = contents[i].split(',');
+        if (fields.has(data[3]) === false) {
+          fields.set(data[3], []);
+        }
+        fields.get(data[3]).push(data[0]);
+        NumStd += 1;
+      }
+    }
+    console.log(`Number of students: ${NumStd}`);
+    for (const [index, field] of fields.entries()) {
+      let text = field.join(', ');
+      text = `Number of students in ${index}: ${field.length}. List: ${text}`;
+      console.log(text);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
