76db837f9a28a5ea82a54d65cf60bcd4f6cbeae6
badrxd <badrxdxd@gmail.com>
2024-02-26 19:33:29 +0100

    lkhra

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n")
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', function (data) {
     process.stdout.write('Your name is: ' + data);
     process.exit();
   });
-
 } else {
   process.stdin.on('data', function (data) {
     process.stdout.write('Your name is: ' + data);
