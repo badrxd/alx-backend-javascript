55b6fcf5ac366026db95526ebd2517dc3dfcd2f4
badrxd <badrxdxd@gmail.com>
2024-02-26 19:29:48 +0100

    task 1. Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n")
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', function (data) {
+    process.stdout.write('Your name is: ' + data);
+    process.exit();
+  })
+
+} else {
+  process.stdin.on('data', function (data) {
+    process.stdout.write('Your name is: ' + data);
+    process.exit();
+  })
+  process.on('exit', function () {
+    process.stdout.write("This important software is now closing\n");
+  })
+}
