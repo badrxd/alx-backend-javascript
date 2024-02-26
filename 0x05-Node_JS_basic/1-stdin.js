ba5c73d08f7eabce8faf5797c612af57abfd8ab2
badrxd <badrxdxd@gmail.com>
2024-02-26 19:31:45 +0100

    add semicolon

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,14 +4,14 @@ if (process.stdin.isTTY) {
   process.stdin.on('data', function (data) {
     process.stdout.write('Your name is: ' + data);
     process.exit();
-  })
+  });
 
 } else {
   process.stdin.on('data', function (data) {
     process.stdout.write('Your name is: ' + data);
     process.exit();
-  })
+  });
   process.on('exit', function () {
     process.stdout.write("This important software is now closing\n");
-  })
+  });
 }
