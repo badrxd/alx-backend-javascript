f744defc194da9dc4fd055a932c6cd85db412a8c
badrxd <badrxdxd@gmail.com>
2024-02-27 01:04:26 +0100

    export app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,11 +2,13 @@ const http = require('node:http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = app;
