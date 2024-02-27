55fc6fc424b2a702ecef4dc120914eec2fed787e
badrxd <badrxdxd@gmail.com>
2024-02-27 19:55:15 +0100

    task 5. Create a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const { readFile } = require('fs').promises;
+
+const countStudents = async () => {
+  try {
+    const fields = new Map();
+    let NumStd = 0;
+    const globalText = [];
+    let contents = await readFile(process.argv[2], 'utf8');
+    contents = contents.split('\n');
+    for (let i = 0; i < contents.length; i += 1) {
+      if (
+        contents[i].length > 0
+        && contents[i] !== 'firstname,lastname,age,field'
+      ) {
+        const data = contents[i].split(',');
+        if (fields.has(data[3]) === false) {
+          fields.set(data[3], []);
+        }
+        fields.get(data[3]).push(data[0]);
+        NumStd += 1;
+      }
+    }
+    // console.log(`Number of students: ${NumStd}`);
+    globalText.push(`Number of students: ${NumStd}`);
+    for (const [index, field] of fields.entries()) {
+      let text = field.join(', ');
+      text = `Number of students in ${index}: ${field.length}. List: ${text}`;
+      globalText.push(text);
+    }
+    return globalText;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!\n');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(res)
+      .then((data) => {
+        data.forEach((e) => {
+          res.write(`${e}\n`);
+        });
+        res.end();
+      })
+      .catch((err) => {
+        res.statusCode = 404;
+        res.end(err);
+      });
+  }
+});
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
