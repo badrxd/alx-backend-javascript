05a7bdfa5b0f3421d6af75e2f20f7932bd0668f7
badrxd <badrxdxd@gmail.com>
2024-02-28 19:29:56 +0100

    0. Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,14 @@
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
+describe('calculateNumber', function () {
+  it('Tests around the “rounded” part', function () {
+    let number = calculateNumber(1, 3);
+    assert.equal(number, 4);
+    number = calculateNumber(1, 3.7);
+    assert.equal(number, 5);
+    number = calculateNumber(1.2, 3.7);
+    assert.equal(number, 5);
+    number = calculateNumber(1.5, 3.7);
+    assert.equal(number, 6);
+  });
+});
