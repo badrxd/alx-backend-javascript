bf4c0db58fb3bcec9e5ce5b1a8bbff85b73a7151
badrxd <badrxdxd@gmail.com>
2024-02-28 19:58:40 +0100

    task 1. Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,20 @@
+const calculateNumber = require('./1-calcul');
+const assert = require('assert');
+describe('calculateNumber', function () {
+  it('Tests around the “rounded” part and the type of calculation', function () {
+    let number = calculateNumber('SUM', 1, 3);
+    assert.equal(number, 4);
+    number = calculateNumber('SUM', 1.5, -3.7);
+    assert.equal(number, -2);
+
+    number = calculateNumber('SUBTRACT', 1.5, -3.7);
+    assert.equal(number, 6);
+    number = calculateNumber('SUBTRACT', -1.5, -3.7);
+    assert.equal(number, 3);
+
+    number = calculateNumber('DIVIDE', 1.4, 4.5);
+    assert.equal(number, 0.2);
+    number = calculateNumber('DIVIDE', 1.5, 0);
+    assert.equal(number, 'Error');
+  });
+});
