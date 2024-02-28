4f7cc3797774fa081ff3c2cb63952b02b2896471
badrxd <badrxdxd@gmail.com>
2024-02-28 21:55:18 +0100

    change comment

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -6,41 +6,41 @@ const { describe, it } = require('mocha');
 describe('calculateNumber', function () {
   let number = undefined;
   describe('SUM', function () {
-    it('checking if return 4', function () {
+    it('checking if numbers round', function () {
       number = calculateNumber('SUM', 1, 3);
       expect(number).to.equal(4);
     });
-    it('checking if return 4', function () {
+    it('checking if numbers round', function () {
       number = calculateNumber('SUM', 1.1, 3.6);
       expect(number).to.equal(5);
     });
   });
 
   describe('SUBTRACT', function () {
-    it('checking if return 6', function () {
+    it('checking if numbers round', function () {
       number = calculateNumber('SUBTRACT', 1.5, -3.7);
       expect(number).to.equal(6);
     });
-    it('checking if return 6', function () {
+    it('checking if numbers round', function () {
       number = calculateNumber('SUBTRACT', 2.5, 3.7);
       expect(number).to.equal(-1);
     });
-    it('checking if return -1', function () {
+    it('checking if numbers round', function () {
       number = calculateNumber('SUBTRACT', -5.5, -3.7);
       expect(number).to.equal(-1);
     });
   });
 
   describe('DIVIDE', function () {
-    it('checking if return 0.2', function () {
+    it('checking if numbers round', function () {
       number = calculateNumber('DIVIDE', 1.4, 4.5);
       expect(number).to.equal(0.2);
     });
-    it('checking if return -1', function () {
+    it('checking if numbers round', function () {
       number = calculateNumber('DIVIDE', -9.6, 1.5);
       expect(number).to.equal(-5);
     });
-    it('checking if return Rrror', function () {
+    it('checking if numbers round', function () {
       number = calculateNumber('DIVIDE', 1.5, 0);
       expect(number).to.equal('Error');
     });
