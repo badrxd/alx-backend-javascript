bf9cdfc103d81e4bf0bc8ef8c4935ce9358f3c44
badrxd <badrxdxd@gmail.com>
2024-02-28 20:35:47 +0100

    task 2. Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,12 +3,12 @@ const expect = require('chai').expect;
 
 describe('calculateNumber', function () {
   it('Tests around the “rounded” part and the type of calculation', function () {
-    it('checking if return -1', function () {
+    it('checking if return 4', function () {
       let number = calculateNumber('SUM', 1, 3);
       expect(number).to.equal(4);
     });
 
-    it('checking if return -1', function () {
+    it('checking if return 6', function () {
       number = calculateNumber('SUBTRACT', 1.5, -3.7);
       expect(number).to.equal(6);
     });
@@ -17,7 +17,7 @@ describe('calculateNumber', function () {
       expect(number).to.equal(-1);
     });
 
-    it('checking if return -1', function () {
+    it('checking if return 0.2', function () {
       number = calculateNumber('DIVIDE', 1.4, 4.5);
       expect(number).to.equal(0.2);
     });
