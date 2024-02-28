d45503b5765ea3d32d90a80e33e534fca863710a
badrxd <badrxdxd@gmail.com>
2024-02-28 20:47:46 +0100

    fix again test

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -9,12 +9,20 @@ describe('calculateNumber', function () {
       number = calculateNumber('SUM', 1, 3);
       expect(number).to.equal(4);
     });
+    it('checking if return 4', function () {
+      number = calculateNumber('SUM', 1.1, 3.6);
+      expect(number).to.equal(5);
+    });
   });
 
   describe('SUBTRACT', function () {
     it('checking if return 6', function () {
       number = calculateNumber('SUBTRACT', 1.5, -3.7);
       expect(number).to.equal(6);
+    });
+    it('checking if return 6', function () {
+      number = calculateNumber('SUBTRACT', 2.5, 3.7);
+      expect(number).to.equal(-1);
     });
     it('checking if return -1', function () {
       number = calculateNumber('SUBTRACT', -5.5, -3.7);
@@ -28,12 +36,8 @@ describe('calculateNumber', function () {
       expect(number).to.equal(0.2);
     });
     it('checking if return -1', function () {
-      number = calculateNumber('DIVIDE', -9.6, 10);
-      expect(number).to.equal(-1);
-    });
-    it('checking if return -1', function () {
-      number = calculateNumber('DIVIDE', 9.6, -10);
-      expect(number).to.equal(-1);
+      number = calculateNumber('DIVIDE', -9.6, 1.5);
+      expect(number).to.equal(-5);
     });
     it('checking if return Rrror', function () {
       number = calculateNumber('DIVIDE', 1.5, 0);
