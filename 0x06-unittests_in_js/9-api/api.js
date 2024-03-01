465949fa14d628852a8efe649214bdd00397f052
badrxd <badrxdxd@gmail.com>
2024-03-01 03:03:24 +0100

    task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,16 +6,8 @@ const port = 7865;
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the payment system');
 });
-app.get('/cart/:id', (req, res) => {
-  const { id } = req.params;
-  try {
-    if (!Number(id)) {
-      return res.status(404).send('');
-    }
-    return res.status(200).send(`Payment methods for cart :${Number(id)}`);
-  } catch (err) {
-    return res.status(404);
-  }
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.status(200).send(`Payment methods for cart :${req.params.id}`);
 });
 
 app.listen(port, () => {
